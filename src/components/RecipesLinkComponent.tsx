4e47524c38c4be60aa07be249f9f20e5e8331971
ludwigwagberg03 <ludwig.wagberg@gmail.com>
2026-03-30 11:52:33 +0200

    img

--- src/components/RecipesLinkComponent.tsx
+++ src/components/RecipesLinkComponent.tsx
@@ -4,7 +4,7 @@ function RecipesLinkComponent() {
     return (
         <div className="flex gap-2 items-center justify-center">
             <Link to="recipes">View all Recipes</Link>
-            <img src="public/right-arrow-svgrepo-com.svg" alt="" className="h-4 w-4 items-center justify-center" />
+            <img src="/right-arrow-svgrepo-com.svg" alt="" className="h-4 w-4 items-center justify-center" />
         </div>
     )
 }
